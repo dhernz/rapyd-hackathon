994ca6627d167bd70bcff58c01094fc09a96d874
Konrad Gnat <konradmgnat@gmail.com>
2022-07-26 22:48:21 -0400

    import font family, header and signup.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,11 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header';
+import SignUp from '../components/SignUp';
 
 const Home: NextPage = () => {
   return (
     <div className="container">
       <Header />
+      <SignUp />
     </div>
   )
 }
