3dfa787e25136af0bc961071a08294026e1ca870
Konrad Gnat <konradmgnat@gmail.com>
2022-07-29 03:10:28 -0400

    orders page

--- frontend/components/TablesTableRow.tsx
+++ frontend/components/TablesTableRow.tsx
@@ -30,7 +30,7 @@ import {
         var hour = a.getHours();
         var min = a.getMinutes();
         var sec = a.getSeconds();
-        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+        var time = date + ' ' + month + ' ' + year + ' ';
         return time;
       }
     
